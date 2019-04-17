7974ec5ff54a46f4ca7b9f30b1d1b8c1ebcae6c2
Andrey Sytnik <ilvif666@gmail.com>
2019-04-18 02:55:06 +0300

    +Vue.component

--- 2.js
+++ 2.js
@@ -8,4 +8,22 @@ var app5 = new Vue({
       this.message = this.message.split('').reverse().join('')
     }
   }
-})
+});
+
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li> {{ todo.text }} </li>'
+});
+
+
+var app6 = new Vue ({
+  el: '#app-6',
+  data: {
+    groceryList: [
+      { id: 0, text: "овощи"},
+      { id: 1, text: "myaso"},
+      { id: 2, text: "frukty"},
+      { id: 3, text: "hleb"}
+    ]
+  }
+});
